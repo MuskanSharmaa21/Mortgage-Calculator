6d19e0f45597a526f334fe39d62a0c81d81e4178
MuskanSharmaa21 <mmuskansharma2110@gmail.com>
2024-08-27 23:25:03 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let finalRepay;
+amount=15000;
+rate=2.5;
+time=5;
+function totalCalculation(amount,rate,time){
+    finalRepay=(amount*rate*time)/100;
+    function display();
+    return finalRepay;
+}
+function display(){
+ 
+}
